d53ff32fdc31d1062a7e68fc6fe54994f06e8d11
Jian Liu <onanying@126.com>
2019-08-20 17:58:56 +0800

    上传

---
+++ src/ListenerInterface.php
@@ -0,0 +1,26 @@
+<?php declare(strict_types=1);
+
+namespace Mix\Event;
+
+/**
+ * Interface ListenerInterface
+ * @package Mix\Event
+ * @author liu,jian <coder.keda@gmail.com>
+ */
+interface ListenerInterface
+{
+
+    /**
+     * 监听的事件
+     * @return array
+     */
+    public function events(): array;
+
+    /**
+     * 处理事件
+     * @param object $event
+     * @return mixed
+     */
+    public function process(object $event);
+
+}
